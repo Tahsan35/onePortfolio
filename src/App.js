82331359c50904b60de9b3df4a63bfd6dd177039
Tahsan <tahsanknmas14@gmail.com>
2022-10-07 12:26:14 +0600

    added social link

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Home from './Components/Home/Home';
 import Navbar from './Components/Navbar/Navbar';
+import SocialLinks from './Components/SocialLInks/SocialLinks';
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
       <Home></Home>
+      <SocialLinks></SocialLinks>
     </div>
   );
 }
